85c5921ae9f8c840964e265530770f4386e44d47
SamueldeBamuel <samuel.de.boer@gmail.com>
2026-01-05 23:32:42 +0100

    crap

--- katten/Assets/Scripts/FORWARDDAY.cs
+++ katten/Assets/Scripts/FORWARDDAY.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class FORWARDDAY : MonoBehaviour
 {
+    public Button PijltjeForward;
+    public TextMeshProUGUI Counter;
+    public int DayCount;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        DayCount = 0;
+        UpdateDayText();
+        PijltjeForward.onClick.AddListener(AddDay);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update is called once per frame  
+    void AddDay()
     {
-        
+        DayCount += 1;
+        UpdateDayText();
+        Debug.Log("Day: " + DayCount);  
+    }
+
+    void UpdateDayText()
+    {
+        Counter.text = DayCount.ToString();
     }
 }
