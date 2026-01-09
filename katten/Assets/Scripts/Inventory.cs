56bf2510cdc47051d6bb1acf167707faed955715
SamueldeBamuel <samuel.de.boer@gmail.com>
2026-01-09 21:27:54 +0100

    KKKKKKKKKKKKKKKKKKKKKKKKKKRRRRRRRRRRRR

---
+++ katten/Assets/Scripts/Inventory.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    Vector2 endPos;
+    Vector2 hiddenPos;
+
+    public RectTransform imageRect;
+    public Button openButton;
+
+    public float duration = 0.35f;
+    public float startXOffset = 400f;
+    Coroutine anim;
+    bool shown;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Awake()
+    {
+        endPos = imageRect.anchoredPosition;
+        hiddenPos = endPos + Vector2.right * startXOffset;
+
+        imageRect.anchoredPosition = hiddenPos;
+
+        openButton.onClick.AddListener(Toggle);
+    }
+
+    void Toggle()
+    {
+        shown = !shown;
+
+        StartAnim(shown ? endPos : hiddenPos);
+    }
+
+    void StartAnim(Vector2 target)
+    {
+        if (anim != null) StopCoroutine(anim);
+        anim = StartCoroutine(AnimateTo(target));
+    }
+
+    IEnumerator AnimateTo(Vector2 target)
+    {
+        Vector2 start = imageRect.anchoredPosition;
+        float t = 0f;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            float s = Mathf.SmoothStep(0f, 1f, t / duration);
+            imageRect.anchoredPosition = Vector2.Lerp(start, target, s);
+            yield return null;
+        }
+
+        imageRect.anchoredPosition = target;
+        anim = null;
+    }
+} 
